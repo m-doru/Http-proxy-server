40253e02eaaffb8678f08311b8fb543c3333a2c5
m-doru <doru.musuroi@gmail.com>
2016-05-02 21:46:27 +0300

    Implement and test RequestAnswerHeader

---
+++ src/main/java/pao/mdoru/impl/HttpRequestAnswerHeader.java
@@ -0,0 +1,63 @@
+package pao.mdoru.impl;
+
+import pao.mdoru.utils.LineInputStream;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by m-doru on 02.05.2016.
+ */
+public class HttpRequestAnswerHeader {
+    private List<String> parameters = new ArrayList<>();
+    private int status;
+    private String message;
+    private String version;
+    public void parseHeader(InputStream serverInput) throws IOException {
+        String firstLine = LineInputStream.readLine(serverInput);
+        this.parseFirstLine(firstLine);
+
+        this.parameters.add(firstLine);
+
+        String line;
+        while((line = LineInputStream.readLine(serverInput)) != null){
+            if(line.length() == 0)
+                break;
+            this.parameters.add(line);
+        }
+    }
+
+    private void parseFirstLine(String line){
+        String[] data = line.split(" ");
+        if(data.length >= 3) {
+            this.version = data[0];
+            this.status = Integer.parseInt(data[1]);
+            StringBuilder messageBuilder = new StringBuilder();
+            for(int i = 2; i < data.length; ++i)
+                messageBuilder.append(data[i] + " ");
+            this.message = messageBuilder.toString().trim();
+        }
+    }
+
+    public void addParameter(String parameter){
+        this.parameters.add(parameter);
+    }
+    public List<String> getParameters(){
+        return this.parameters;
+    }
+
+    public int getStatusCode(){
+        return this.status;
+    }
+
+    public String getMessage(){
+        return this.message;
+    }
+
+    public String getVersion(){
+        return this.version;
+    }
+}
---
+++ src/test/java/pao/mdoru/impl/HttpRequestAnswerHeaderTest.java
@@ -0,0 +1,48 @@
+package pao.mdoru.impl;
+
+import org.junit.Test;
+
+import java.io.BufferedReader;
+import java.io.ByteArrayInputStream;
+import java.io.StringReader;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by m-doru on 02.05.2016.
+ */
+public class HttpRequestAnswerHeaderTest {
+    private String answer = "HTTP/1.1 404 Not Found\n" +
+            "Content-Type: text/html; charset=UTF-8\n" +
+            "Content-Length: 1562\n" +
+            "Date: Mon, 02 May 2016 09:18:59 GMT\n" +
+            "\n" +
+            "<!DOCTYPE html>\n" +
+            "<html lang=en>\n" +
+            "  <meta charset=utf-8>\n" +
+            "  <meta name=viewport content=\"initial-scale=1, minimum-scale=1, width=device-width\">\n" +
+            "  <title>Error 404 (Not Found)!!1</title>\n" +
+            "  <style>\n" +
+            "    *{margin:0;padding:0}html,code{font:15px/22px arial,sans-serif}html{background:#fff;color:#222;padding:15px}body{margin:7% auto 0;max-width:390px;min-height:180px;padding:30px 0 15px}* > body{background:url(//www.google.com/images/errors/robot.png) 100% 5px no-repeat;padding-right:205px}p{margin:11px 0 22px;overflow:hidden}ins{color:#777;text-decoration:none}a img{border:0}@media screen and (max-width:772px){body{background:none;margin-top:0;max-width:none;padding-right:0}}#logo{background:url(//www.google.com/images/branding/googlelogo/1x/googlelogo_color_150x54dp.png) no-repeat;margin-left:-5px}@media only screen and (min-resolution:192dpi){#logo{background:url(//www.google.com/images/branding/googlelogo/2x/googlelogo_color_150x54dp.png) no-repeat 0% 0%/100% 100%;-moz-border-image:url(//www.google.com/images/branding/googlelogo/2x/googlelogo_color_150x54dp.png) 0}}@media only screen and (-webkit-min-device-pixel-ratio:2){#logo{background:url(//www.google.com/images/branding/googlelogo/2x/googlelogo_color_150x54dp.png) no-repeat;-webkit-background-size:100% 100%}}#logo{display:inline-block;height:54px;width:150px}\n" +
+            "  </style>\n" +
+            "  <a href=//www.google.com/><span id=logo aria-label=Google></span></a>\n" +
+            "  <p><b>404.</b> <ins>That’s an error.</ins>\n" +
+            "  <p>The requested URL <code>/l</code> was not found on this server.  <ins>That’s all we know.</ins>";
+
+    @Test
+    public void parseHeader() throws Exception {
+        HttpRequestAnswerHeader header = new HttpRequestAnswerHeader();
+
+        header.parseHeader(new ByteArrayInputStream(answer.getBytes()));
+
+        assertEquals(header.getStatusCode(), 404);
+        assertEquals(header.getMessage(), "Not Found");
+        assertEquals(header.getVersion(), "HTTP/1.1");
+        assertEquals(header.getParameters().size(), 4);
+        assertEquals(header.getParameters().get(0), "HTTP/1.1 404 Not Found");
+        assertEquals(header.getParameters().get(1), "Content-Type: text/html; charset=UTF-8");
+        assertEquals(header.getParameters().get(2), "Content-Length: 1562");
+        assertEquals(header.getParameters().get(3), "Date: Mon, 02 May 2016 09:18:59 GMT");
+    }
+
+}
