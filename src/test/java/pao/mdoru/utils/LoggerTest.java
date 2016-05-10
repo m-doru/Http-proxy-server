863e1eb1db5f23a5400cfb05349e9f04828b5c94
m-doru <doru.musuroi@gmail.com>
2016-05-11 00:24:14 +0300

    Added tearDown method for when asserts fails in LoggerTest

--- src/test/java/pao/mdoru/utils/LoggerTest.java
+++ src/test/java/pao/mdoru/utils/LoggerTest.java
@@ -1,5 +1,6 @@
 package pao.mdoru.utils;
 
+import org.junit.After;
 import org.junit.Test;
 
 import java.nio.file.FileSystems;
@@ -13,13 +14,23 @@ import static org.junit.Assert.*;
  * Created by m-doru on 02.05.2016.
  */
 public class LoggerTest {
+    Path dirPath;
+    Path filePath;
+
+    @After
+    public void tearDown() throws Exception{
+        Files.delete(filePath);
+
+        Files.delete(dirPath);
+    }
+
     @Test
     public void log_someData_writesDataToTheLogFile() throws Exception {
         String directory = "directory";
         String fileName = "fileLog";
 
-        Path dirPath = FileSystems.getDefault().getPath(directory);
-        Path filePath = FileSystems.getDefault().getPath(directory, fileName);
+        dirPath = FileSystems.getDefault().getPath(directory);
+        filePath = FileSystems.getDefault().getPath(directory, fileName);
 
         assertFalse(Files.exists(dirPath));
         assertFalse(Files.exists(filePath));
@@ -34,11 +45,7 @@ public class LoggerTest {
 
         String fileLogContent = new String(Files.readAllBytes(filePath));
 
-        assertEquals("test", fileLogContent);
-
-        Files.delete(filePath);
-
-        Files.delete(dirPath);
+        assertEquals("test\n", fileLogContent);
     }
 
 }
